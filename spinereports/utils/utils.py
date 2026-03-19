fix wrong index canal

spinereports/utils/utils.py
@@ -217,7 +217,7 @@ def _properties2d(canal, spinalcord, dim, radius=2):
     # Compute area
     area_canal = np.sum(canal) * dim[0] * dim[1]
     right_area_canal = np.sum(canal[:canal.shape[0]//2+1, :]) * dim[0] * dim[1]
-    left_area_canal = np.sum(canal[canal.shape[1]//2:, :]) * dim[0] * dim[1]
+    left_area_canal = np.sum(canal[canal.shape[0]//2:, :]) * dim[0] * dim[1]
 
     # Compute eccentricity
     if diameter_AP_canal < diameter_RL_canal: